32cdc3b0c11712a685971ba5e3a3fba04166df4b
farkmu45 <farukmicros@outlook.com>
2022-05-09 18:59:20 +0700

    Hash password on create

--- app/Filament/Resources/AdminResource/Pages/CreateAdmin.php
+++ app/Filament/Resources/AdminResource/Pages/CreateAdmin.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\AdminResource\Pages;
 
 use App\Filament\Resources\AdminResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Hash;
 
 class CreateAdmin extends CreateRecord
 {
@@ -12,5 +13,11 @@ class CreateAdmin extends CreateRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['password'] = Hash::make($data['password']);
+        return $data;
     }
 }
--- app/Filament/Resources/AdminResource/Pages/EditAdmin.php
+++ app/Filament/Resources/AdminResource/Pages/EditAdmin.php
@@ -4,8 +4,16 @@ namespace App\Filament\Resources\AdminResource\Pages;
 
 use App\Filament\Resources\AdminResource;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Hash;
 
 class EditAdmin extends EditRecord
 {
     protected static string $resource = AdminResource::class;
+
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $data['password'] = Hash::make( $data['password']);
+        return $data;
+    }
 }
