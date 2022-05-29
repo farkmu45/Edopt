0f8f5d11124593933e5df9916a0d0295f6a46d6d
farkmu45 <farukmicros@outlook.com>
2022-05-29 09:16:33 +0700

    feat: Add page limit in article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,14 +5,13 @@ namespace App\Http\Controllers;
 use App\Http\Resources\ArticleCollection;
 use App\Http\Resources\ArticleResource;
 use App\Models\Article;
-use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function getAll(): ArticleCollection
     {
         return new ArticleCollection(Article::paginate(
-            5,
+            request('per_page', 5),
             ['id', 'admin_id', 'title', 'preview_text', 'image_url', 'created_at', 'updated_at']
         ));
     }
