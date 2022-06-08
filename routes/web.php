1b342c098b4e8200604483c68db26aa94ce0dce4
farkmu45 <farukmicros@outlook.com>
2022-06-08 19:46:38 +0700

    fix: Add route for linking storage

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::redirect('/login', '/admin/login')->name('login');
+
+Route::get('/link', function () {
+    Artisan::call('storage:link');
+});
